e307f0e65d5d69357169edf291eb38e943d45204
olatoye-Brain <sodimuolatoye@gmail.com>
2022-12-30 09:02:27 +0100

    newsroom server api

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+const router = express.Router();
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+
+app.use(express.json())
+app.use(cors())
+
+app.use(express.urlencoded({extended: false}))
+// app.use(express.urlencoded({limit: '25mb'}));
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(router)
+
+
+const NewsAPI = require('newsapi')
+const newsapi = new NewsAPI(process.env.KEY);
+
+// To query top headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
+router.get('/top-news', (req, res)=>{
+    newsapi.v2.topHeadlines({
+      category: 'general',
+      language: 'en',
+      country: 'us',
+      pageSize: 12
+    }).then(response => {
+      res.status(200).json(response.articles)
+    });
+})
+
+router.get('/top-news/:category', (req, res) => {
+    const { category } = req.params
+        newsapi.v2.topHeadlines({
+        category: category,
+        language: 'en',
+        country: 'us',
+    }).then(response => {
+        res.status(200).json(response.articles)
+    });
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`App is listening to ${PORT}`)
+} )
